e4af318d055e571df6e664653544c541bf5af2ee
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-10-14 23:44:01 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,15 @@
--- Print full description of the table 'Books'
 SELECT 
-    COLUMN_NAME AS 'Column',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Nullable',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'Books'
-ORDER BY ORDINAL_POSITION;
+    TABLE_NAME,
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_NAME = 'books'
+    AND TABLE_SCHEMA = DATABASE();
 
 
