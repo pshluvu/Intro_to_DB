2d3b2148a966b3e9782cead670d590125601dc26
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-10-14 23:48:23 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,15 @@
--- Print full description of the table 'books'
-SHOW COLUMNS FROM books;
+-- Print full description of all tables in the current database
+SELECT 
+    TABLE_NAME AS 'Table',
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+ORDER BY TABLE_NAME, ORDINAL_POSITION;
+
 
 
