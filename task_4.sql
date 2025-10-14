070eb72d13903fd79a08aba3773bdfe5e2b25f66
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-10-14 23:58:27 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,15 @@
--- Print full description of the table 'books'
-SHOW COLUMNS FROM books;
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
 
 
 
