f8433ba7c7347e17d911eae5618d364ba3d3fe1d
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-10-14 23:41:31 +0200

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+USE alx_book_store;
+
+SELECT 
+    TABLE_NAME,
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
